c4497e9c04298f13d32a76e43fd5703e69c2e846
SantianoLaptop <santianogames@gmial.com>
2026-08-14 18:15:52 +0300

    дополнение сущностей

---
+++ VeloSportStore.API/Domain/Entities/ProductAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using VeloSportStore.API.Domain.Enums.Types;
+
+namespace VeloSportStore.API.Domain.Entities
+{
+    public class ProductAttribute
+    {
+        public Guid Id { get; set; }
+        public string Key { get; set; }
+        public ProductAttributeTypes ValueType { get; set; }
+        public string Unit { get; set; } = string.Empty;
+    }
+}
---
+++ VeloSportStore.API/Domain/Enums/Types/ProductAttributeTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace VeloSportStore.API.Domain.Enums.Types
+{
+    public enum ProductAttributeTypes
+    {
+        Uuid,       // id товара
+        Count,      // шт
+        Metrics,    // кг, м, сек
+        Price,      // rub, usd
+        Text        // дисковые тормоза
+    }
+}
